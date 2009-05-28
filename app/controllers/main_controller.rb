1dbf1a3e629495348adf5a097e4b9fa8d1527cf0
Daniel Morrison <daniel@collectiveidea.com>
2009-05-28 11:24:19 -0400

    Pull in some feeds with rfeedparser

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
+require 'rfeedparser'
+
 class MainController < ApplicationController
   def index
     @clocked_in_users=User.clocked_in
     @clocked_out_users=User.clocked_out
+    
+    @feed = FeedParser.parse('http://keith.talho.org/api/statuses/public_timeline.atom')
   end
 
 end
