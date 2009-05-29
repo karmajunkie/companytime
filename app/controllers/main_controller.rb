9b210d8d01d47acee89e2740cca5795992994aa3
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-05-29 08:19:56 -0500

    added better UI support for laconica feeds and a config file for the laconica uri (and future options)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,9 @@ class MainController < ApplicationController
   def index
     @clocked_in_users=User.clocked_in
     @clocked_out_users=User.clocked_out
-    
-    @feed = Rails.cache.fetch(Time.zone.now.strftime("feed-%Y-%m-%d-%H")) do
-        FeedParser.parse('http://keith.talho.org/api/statuses/public_timeline.atom')
+    cache_id=Time.zone.now.strftime("feed-%Y-%m-%d-%H-")+((Time.zone.now.strftime("%M").to_i / 20).round * 20).to_s
+    @feed = Rails.cache.fetch(cache_id) do
+        FeedParser.parse(LACONICA_URI)
     end
   end
 
