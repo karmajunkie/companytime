a6f42f5bef38f2401a9cac8da2270a8d2260e774
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-06-08 23:27:48 -0500

    more support for jquery date field

--- public/javascripts/leave_request.js
+++ public/javascripts/leave_request.js
@@ -11,18 +11,38 @@ $(document).ready(function() {
 					updateInputs($(this).attr("id"), selectedDate);
 				}
 			);
+    $("#leave_period_all_day").bind("change", function(e){
+        if(!$(this).attr("checked")){
+            $("#leave_period_from_date_4i").attr("disabled", false);
+            $("#leave_period_from_date_5i").attr("disabled", false);
+            $("#leave_period_from_date_6i").attr("disabled", false);
+            $("#leave_period_until_date_4i").attr("disabled", false);
+            $("#leave_period_until_date_5i").attr("disabled", false);
+            $("#leave_period_until_date_6i").attr("disabled", false);
+        }else{
+            $("#leave_period_from_date_4i").attr("disabled", true);
+            $("#leave_period_from_date_5i").attr("disabled", true);
+            $("#leave_period_from_date_6i").attr("disabled", true);
+            $("#leave_period_until_date_4i").attr("disabled", true);
+            $("#leave_period_until_date_5i").attr("disabled", true);
+            $("#leave_period_until_date_6i").attr("disabled", true);
+        }
+    });
+    updateInputs("from_date", new Date());
+    updateInputs("until_date", new Date());
 });
 function updateInputs(type, selectedDate) {
-	alert(type);
-	$("#"+type+"_time_1i").val(selectedDate.getFullYear());
-	$("#"+type+"_time_2i").val(selectedDate.getMonth());
-	$("#"+type+"_time_3i").val(selectedDate.getDate());
+
+	$("#leave_period_"+type+"_1i").val(selectedDate.getFullYear());
+	$("#leave_period_"+type+"_2i").val(selectedDate.getMonth());
+	$("#leave_period_"+type+"_3i").val(selectedDate.getDate());
 }
 function openNewLeavePeriodDlg() {
 	$("#leave_period_form_dlg").slideToggle();
 }
 function addNewLeavePeriod() {
 	$('#leave_period_form_dlg').slideUp();
+    
 }
 function closeNewLeavePeriodDlg() {
 	$('#leave_period_form_dlg').slideUp();
