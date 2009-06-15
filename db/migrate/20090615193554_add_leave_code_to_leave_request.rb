46a71e7f65285b99750b0af2a45de6be3992b780
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-06-15 16:36:07 -0500

    add leave code to leave requests to denote what type of leave this is covered by

--- db/migrate/20090615193554_add_leave_code_to_leave_request.rb
+++ db/migrate/20090615193554_add_leave_code_to_leave_request.rb
@@ -1,7 +1,9 @@
 class AddLeaveCodeToLeaveRequest < ActiveRecord::Migration
   def self.up
+    add_column :leave_requests, :leave_code, :string
   end
 
   def self.down
+    remove_column :leave_requests, :leave_code
   end
 end
