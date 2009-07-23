e8686f7159c04ee60c4e61c97dd60541a686c845
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-07-23 09:34:41 -0500

    fixed a timeout bug from the missing laconica server

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,9 +5,9 @@ class MainController < ApplicationController
     @clocked_in_users=User.clocked_in
     @clocked_out_users=User.clocked_out
     cache_id=Time.zone.now.strftime("feed-%Y-%m-%d-%H-")+((Time.zone.now.strftime("%M").to_i / 20).round * 20).to_s
-    @feed = Rails.cache.fetch(cache_id) do
-        FeedParser.parse(LACONICA_URI)
-    end  
+#    @feed = Rails.cache.fetch(cache_id) do
+#        FeedParser.parse(LACONICA_URI)
+#    end
   end
 
 end
